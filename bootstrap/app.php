e2b37fda34ef649ce84dabd7c57ac93dcc1eebeb
Wildan <wildanafif00@gmail.com>
2024-06-07 16:32:12 +0700

    Adding middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trimStrings(
+            ['*']
+        );
         $middleware->alias([
             'snap.authentication' => \App\Http\Middleware\SnapAuthentication::class,
             'request.logger' => \App\Http\Middleware\RequestLogger::class,
